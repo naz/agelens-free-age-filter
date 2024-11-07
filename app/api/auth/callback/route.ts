b42a2e5b61f3d89e6cac299c9b4e34449e836cf1
naz <675397+naz@users.noreply.github.com>
2024-11-07 08:28:21 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ app/api/auth/callback/route.ts
@@ -0,0 +1,44 @@
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+import { createClient } from "@/lib/supabase/server";
+import { waitUntil } from "@vercel/functions";
+import { dub } from "@/lib/dub";
+
+export async function GET(request: Request) {
+  const { searchParams, origin } = new URL(request.url);
+  const code = searchParams.get("code");
+  // if "next" is in param, use it as the redirect URL
+  const next = searchParams.get("next") ?? "/";
+
+  if (code) {
+    const supabase = createClient(cookies());
+    const { data, error } = await supabase.auth.exchangeCodeForSession(code);
+    if (!error) {
+      const { user } = data;
+      const clickId =
+        cookies().get("dub_id")?.value || cookies().get("dclid")?.value;
+      const isNewUser =
+        new Date(user.created_at) > new Date(Date.now() - 10 * 60 * 1000);
+      // if the user is new and has a clickId cookie, track the lead
+      if (clickId && isNewUser) {
+        waitUntil(
+          dub.track.lead({
+            clickId,
+            eventName: "Sign Up",
+            customerId: user.id,
+            customerName: user.user_metadata.name,
+            customerEmail: user.email,
+            customerAvatar: user.user_metadata.avatar_url,
+          }),
+        );
+        // delete the clickId cookie
+        cookies().delete("dub_id");
+        cookies().delete("dclid");
+      }
+      return NextResponse.redirect(`${origin}${next}`);
+    }
+  }
+
+  // return the user to an error page with instructions
+  return NextResponse.redirect(`${origin}/auth/auth-code-error`);
+}
